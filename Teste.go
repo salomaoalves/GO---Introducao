c1554ec990bc2a1378fe8e1a6f773603109eec1e
Salomão <salomao.alves222@gmail.com>
2018-11-14 11:19:22 -0200

    Teste

---
+++ Teste.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+/*func delta(a int, b int, c int) int {
+	d := b*b - 4*a*c
+	return d
+}*/
+
+func main() {
+	tempo := time.Now()
+	fmt.Print(math.Pow(2, 5))
+	/*
+		inputUtilizador := bufio.NewReader(os.Stdin)
+		fmt.Print("A:")
+		a, _ := inputUtilizador.ReadString('\n')
+		fmt.Print("B:")
+		var b, _ int = int(inputUtilizador.ReadString('\n'))
+		fmt.Print("C:")
+		c := inputUtilizador.ReadLine('\n')
+		d := delta(a, b, c)*/
+}
